fb96dc651304fdc36589febaf4a205969d92d91d
Hirni-Meshram3 <84446123+Hirni-Meshram3@users.noreply.github.com>
2021-05-26 10:34:21 +0530

    Update build.bazel.sh

--- .github/workflows/build.bazel.sh
+++ .github/workflows/build.bazel.sh
@@ -21,6 +21,7 @@ export BAZEL_OS=$(uname | tr '[:upper:]' '[:lower:]')
 export BAZEL_VERSION=$(cat .bazelversion)
 if [ `uname -m` == "aarch64" ]; then
    sudo wget -o /usr/bin/bazel https://github.com/bazelbuild/bazel/releases/download/3.7.2/bazel-3.7.2-linux-arm64
+   sudo chmod u+x /usr/bin/bazel
    sudo bazel info release
    #sudo cp ./install /usr/local/bin/bazel
    #sudo chmod u+x /usr/local/bin/bazel
